d4177e3ad66f9198ab76dd5a57b1ba7910435e2a
b3LL3 <157781526+bouncybellybelle@users.noreply.github.com>
2026-04-24 19:43:55 +0800

    Update TicketFactoryContract.sol
    manualy copy pasted da factory bc merge not working propoerly

--- contracts/TicketFactoryContract.sol
+++ contracts/TicketFactoryContract.sol
@@ -8,11 +8,27 @@ contract TicketFactoryContract{
 
     TicketContract[] private deployedEvents;
 
+    address public factoryOwner;
+
     receive() external  payable { }
+
+    constructor() {
+        factoryOwner = msg.sender;
+    }
+
+    modifier onlyFactoryOwner(){
+        require(msg.sender == factoryOwner, "Not factory owner");
+        _;
+    }
 
     function createEvent(string memory _eventName, uint _eventDate, uint _ticketPrice, uint _maxTickets) public {
         TicketContract newEvent = new TicketContract(msg.sender, _eventName, _eventDate, _ticketPrice, _maxTickets);
         deployedEvents.push(newEvent);
+    }
+
+    function closeEvent(address _eventContract) public onlyFactoryOwner{
+        TicketContract eventContract = TicketContract(_eventContract);
+        eventContract.closeEvent();
     }
 
     function getEvents() public view returns (TicketContract[] memory) {
