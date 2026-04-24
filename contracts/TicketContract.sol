e01760f357922742c9ac88f56cb3c6d0a8cc2a9f
waleedlugod <lugodwaleed@gmail.com>
2026-04-24 14:40:17 +0800

    belle code

--- contracts/TicketContract.sol
+++ contracts/TicketContract.sol
@@ -1,3 +1,36 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
+
+contract TicketContract {
+
+    address parentContract;
+    address public organizer;
+
+    mapping(uint => address) public ticketOwner;
+
+    string public eventName;
+    uint public eventDate;
+    uint public ticketPrice;
+
+    enum Status { OPEN, SOLD_OUT }
+    Status public status;
+
+
+    modifier onlyTicketOwner(uint _ticketID){
+        require(ticketOwner[_ticketID] == msg.sender, "You are not the ticket owner!");
+        _;
+    }
+
+    constructor (address _organizer, string memory _eventName, uint _eventDate, uint _ticketPrice){
+        parentContract = msg.sender;
+        organizer = _organizer;
+        eventName = _eventName;
+        eventDate = _eventDate;
+        ticketPrice = _ticketPrice;
+        status = Status.OPEN;
+    }
+
+}
+
+
--- contracts/TicketFactoryContract.sol
+++ contracts/TicketFactoryContract.sol
@@ -3,3 +3,16 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 import "contracts/TicketContract.sol";
+
+contract TicketFactoryContract{
+
+    TicketContract[] private deployedEvents;
+
+    receive() external  payable { }
+
+    function createEvent(string memory _eventName, uint _eventDate, uint _ticketPrice) public {
+        TicketContract newEvent = new TicketContract(msg.sender, _eventName, _eventDate, _ticketPrice);
+        deployedEvents.push(newEvent);
+    }
+
+}
